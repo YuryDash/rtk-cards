27b9f8ea9fc1e3227d5305ffb61798d9d2fa9791
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-05-27 16:19:04 +0300

    123123123

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import { decrement, increment, incrementByAmount, incrementAsync, incrementIfOdd, selectCount } from "./counterSlice";
 import styles from "./Counter.module.css";
--- src/index.tsx
+++ src/index.tsx
@@ -5,19 +5,21 @@ import { store } from "./app/store";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
+import { createBrowserRouter, RouterProvider, BrowserRouter } from "react-router-dom";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
+const router = createBrowserRouter([{ path: "" }]);
+
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
