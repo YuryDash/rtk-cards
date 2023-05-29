52502a9bb296c852b90d980de1504e4b1b518bff
Yury Daszukiewicz <ydaszukiewicz@gmail.com>
2023-05-29 21:38:35 +0300

    123

--- src/features/path-recovery/PathRecovery.tsx
+++ src/features/path-recovery/PathRecovery.tsx
@@ -1,13 +1,22 @@
 import { Link } from "react-router-dom";
 import { PATH } from "../counter/common/constants/path";
-
+import s from "./pathRecovery.module.scss";
+import TextField from "@mui/material/TextField/TextField";
+import Button from "@mui/material/Button/Button";
 export const PathRecovery = () => {
   return (
-    <div>
-      <h1>PathRecovery-PathRecovery-PathRecovery-PathRecovery</h1>
-      <button>
-        <Link to={PATH.MAIN}>Back to Main</Link>
-      </button>
+    <div className={s.container}>
+      <div className={s.recovery}>
+        <h1 className={s.recovery__title}>Forgot your password ?</h1>
+        <TextField fullWidth id="standard-basic" label="Email" variant="standard" />
+        <p className={s.recovery__instructions}>Enter your email address and we will send you further instructions</p>
+
+        <Button fullWidth variant="contained">
+          Send instructions
+        </Button>
+        <div className={s.recovery__text}>Did you remember your password?</div>
+        <Link to={PATH.MAIN}>Try logging in</Link>
+      </div>
     </div>
   );
 };
