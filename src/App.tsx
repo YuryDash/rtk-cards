4066718fe826d5a0a875046b6a9af3ccaf8869f9
SempaiDarcy <mr_darcy_1@mail.ru>
2023-05-31 19:45:37 +0500

    delete PassRecovery

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { PATH } from "common/constants/path";
-import { Registration } from "components/auth/registration/Registration";
-import { PathRecovery } from "features/path-recovery/PathRecovery";
 import { Error404 } from "features/error404/Error404";
 import { Header } from "components/header/header";
-import { Login } from "components/auth/Login/Login";
+import { Registration } from "features/auth/registration/Registration";
+import { Login } from "features/auth/Login/Login";
 
 export const App = () => {
   return (
@@ -15,7 +14,6 @@ export const App = () => {
         <Route path="/" element={<Navigate to={PATH.LOGIN} />} />
         <Route path={PATH.LOGIN} element={<Login />} />
         <Route path={PATH.REGISTRATION} element={<Registration />} />
-        <Route path={PATH.PASSWORD_RECOVERY} element={<PathRecovery />} />
         <Route path={PATH.ERROR404} element={<Error404 />} />
       </Routes>
     </div>
